3424953c75caaacae0e7e1cf2b79f8c431a519ec
RomainAndine <romainandine@gmail.com>
2023-10-05 16:59:50 +0200

    Q-3-2-Affichage-des-q-r-sur-vue

--- asset/js/questionnaire.js
+++ asset/js/questionnaire.js
@@ -1,25 +1,65 @@
 import { addElement } from "./modules/addElement.js";
 
-let url = new URL(window.location.href);
-let classe = url.searchParams.get('classe');
-let matiere = url.searchParams.get('matiere');
+const url = new URL(window.location.href);
+const classe = url.searchParams.get("classe");
+const matiere = url.searchParams.get("matiere");
 
+const quizContainer = document.getElementById("quiz-container");
+const btnNext = document.getElementById("btn-next");
 
-    fetch(`asset/json/${classe}/${matiere}.json`)
-        .then(response => response.json())
-        .then(response => {
-            let questionnaire = response.questionnaire;
-            console.log(questionnaire)
-            questionnaire.map(question => {
-                console.log(question.question)
-                let reponse = question.answers;
+async function getQuestionsAndResponsesBySubject(subject) {
+  const response = await fetch(`asset/json/${classe}/${matiere}.json`);
+  const data = await response.json();
 
-                reponse.map(item => {
-                    console.log(item)
-                })
+  return data.questionnaire;
+}
 
-                console.log(question.correctAnswer)
-                
-                
-            })
-        })
+/**
+ * Prend en paramètre un tableau de question
+ * Génère un index aléatoire parmi les questions
+ * Supprime la question récupérée avant de la renvoyée
+ * @param {Question[]} questionnaire
+ */
+function getQuestionRandomly(questionnaire) {
+  let i = Math.floor(Math.random() * questionnaire.length);
+  let question = questionnaire[i];
+  questionnaire.splice(i, 1);
+  return question;
+}
+
+function applyNextQuestion(questionnaire) {
+  // Vérifier que le questionnaire n'est pas vide, si vide, alors faire un return
+  if (questionnaire.length === 0) {
+    console.log("Fin du jeu");
+  //  return;
+  }
+
+  let nextQuestion = getQuestionRandomly(questionnaire);
+  quizContainer.innerHTML = nextQuestion.question;
+}
+
+function isGameOver(questionnaire) {
+  return questionnaire.length === 0;
+}
+
+async function main() {
+  // : Récupérer les questions et réponses du questionnaire
+  const questionnaire = await getQuestionsAndResponsesBySubject(matiere);
+  // Déclarer les variables du jeu : score
+  let score = 0;
+
+  applyNextQuestion(questionnaire);
+
+  // TODO : Réagir aux clicks sur les boutons "Réponse apportée" / "Question suivante"
+  btnNext.addEventListener("click", () => {
+    console.log(questionnaire);
+    if (isGameOver(questionnaire)) {
+      // TODO : Gérer la fin du jeu
+      console.log("Fin du jeu");
+    } else {
+      applyNextQuestion(questionnaire);
+    }
+  });
+}
+
+main();

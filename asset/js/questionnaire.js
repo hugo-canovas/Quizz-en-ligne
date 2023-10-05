0b94789792ef75f5c76c0f790df125049a729fee
hcanovas <hcanovas@ponant.com>
2023-10-05 14:54:23 +0200

    Q-6-2 modification de masse sur les json

--- asset/js/questionnaire.js
+++ asset/js/questionnaire.js
@@ -7,4 +7,19 @@ let matiere = url.searchParams.get('matiere');
 
     fetch(`asset/json/${classe}/${matiere}.json`)
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => {
+            let questionnaire = response.questionnaire;
+            console.log(questionnaire)
+            questionnaire.map(question => {
+                console.log(question.question)
+                let reponse = question.answers;
+
+                reponse.map(item => {
+                    console.log(item)
+                })
+
+                console.log(question.correctAnswer)
+                
+                
+            })
+        })
